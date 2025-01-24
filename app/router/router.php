81916794c7e520bf7c05fc38de0149358688d0d1
Igor <igorpaulo359@gmail.com>
2025-01-24 17:58:51 -0300

    commit

---
+++ app/router/router.php
@@ -0,0 +1,31 @@
+<?php
+
+function routes(){
+    return require 'routes.php';
+}
+
+function exactMatchUri($uri, $routes){
+    if(array_key_exists($uri, $routes)){
+        return [$uri => $routes[$uri]];
+    }
+
+    return [];
+}
+
+function router(){
+    $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+    $routes = routes();
+    
+    $matchedUri = exactMatchUri($uri, $routes);
+
+    if(empty($matchedUri))
+
+    $matchedUri = array_filter(
+        $routes, 
+        function($value) use ($uri){
+            $regex = str_replace('/', '\/', ltrim($value, '/'));
+            return preg_match("/^$regex$/", ltrim($uri, '/'));
+        }, ARRAY_FILTER_USE_KEY
+    );
+}
